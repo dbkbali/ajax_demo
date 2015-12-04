3d4528d5a1e5efd816bda57b663d5f7fa2925b0f
David Krett <dbkrett@gmail.com>
2015-12-03 20:30:57 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+
+Note.create({
+  title: "AJAX - Dont be Intimidated By Acronyms!",
+  description: "stands for Asynchronous Javascript And XML(Extensible Markup Language), who the hell \
+                named this!. Sounds complicated and intimidating! but the concept is really easy. \
+                Instead of completing a full page reload we program the browser to ask the server just to send \
+                a piece of code",
+  reference_url: ""
+})
+
+Note.create({
+  title: "Mastering AJAX with jQuery",
+  description: "For posting and getting data just understand this command.\
+                Most important settings to understand in the command are: \
+                url; method; data; dataType; contentType; and how to handle the different \
+                callbacks.",
+  reference_url: "http://api.jquery.com/jQuery.ajax/"
+})
+
+Note.create({
+  title: "Google Definition - ",
+  description: "Ajax (also AJAX; /ˈeɪdʒæks/; short for asynchronous JavaScript and XML) \
+                 is a set of web development techniques utilizing many web technologies used on \
+                 the client-side to create asynchronous Web applications. With Ajax, web applications \
+                 can send data to and retrieve from a server asynchronously (in the background) without \
+                 interfering with the display and behavior of the existing page. By decoupling the data \
+                 interchange layer from the presentation layer, Ajax allows for web pages, and by extension \
+                 web applications, to change content dynamically without the need to reload the entire page. \
+                  Data can be retrieved using the XMLHttpRequest object. Despite the name, the use of XML is not \
+                  required (JSON is often used in the AJAJ variant), and the requests do not need to be asynchronous",
+  reference_url: "https://en.wikipedia.org/wiki/Ajax_(programming)"
+})
