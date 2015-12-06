a9bb99a6653314493daf8cf7bb063ab759df43f9
David Krett <dbkrett@gmail.com>
2015-12-05 21:42:34 -0500

    tweaks to js

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,67 +1,47 @@
 
 
 $(document).ready(function() {
-   //$.ajax({
-   //  url: "/notes",
-   //  method: "GET",
-   //  dataType: 'json',
-   //  success: function(notes) {
-   //    $(notes).each(function(i, note){
-   //      heading = $('<h2/>').addClass('post-title').text(note.title)
-   //      time = $('<p/>').addClass('post-meta').append($('<span/>')
-   //        .addClass('post-category post-category-design').text('Created: ' + note.created_at))
-   //      header = $("<header/>").addClass('post-header')
-   //                .append(heading).append(time)
-   //      $('#notes').append(
-   //        $("<section/>")
-   //          .append(header)
-   //          .append($('<div/>').addClass("post-description")
-   //            .append($('<p/>').text(note.description)))
-   //            .append($("<a/>").attr('href', note.reference_url).text(note.reference_url)
-   //          ))
-   //      })
-   //    }
 
-   //    // Iterates through our json and renders and
-   //    // appends our notes
+  renderNote = function(note) {
+    heading = $('<h2/>').addClass('post-title').text(note.title)
+    time = $('<p/>').addClass('post-meta').append($('<span/>')
+      .addClass('post-category post-category-design').text('Created: ' + note.created_at))
+    header = $("<header/>").addClass('post-header')
+              .append(heading).append(time)
+    $('#notes').append(
+      $("<section/>").append(header)
+        .append($('<div/>').addClass("post-description")
+          .append($('<p/>').text(note.description)))
+          .append($("<a/>").attr('href', note.reference_url).text(note.reference_url)
+        ));
+      },
 
-   //})
+
+   $.ajax({
+     url: "/notes",
+     method: "GET",
+     dataType: 'json',
+     success: function(notes) {
+       $(notes).each(function(i, note){
+         renderNote(note);
+         $('span.note-count').text($('.post-description').length);
+       })
+      },
+       // Iterates through our json and renders and
+       // appends our note
+   })
 
     // Shorthand command for the above is jQuery.getJSON( url [, data ] [, success ] )
     //
     //
 
-    renderNote = function(note) {
-      heading = $('<h2/>').addClass('post-title').text(note.title)
-      time = $('<p/>').addClass('post-meta').append($('<span/>')
-        .addClass('post-category post-category-design').text('Created: ' + note.created_at))
-      header = $("<header/>").addClass('post-header')
-                .append(heading).append(time)
-      $('#notes').append(
-        $("<section/>").append(header)
-          .append($('<div/>').addClass("post-description")
-            .append($('<p/>').text(note.description)))
-            .append($("<a/>").attr('href', note.reference_url).text(note.reference_url)
-          ));
-        }
 
-    $.getJSON("/notes", function(notes){
-        $(notes).each(function(i, note){
-            renderNote(note)
-            //heading = $('<h2/>').addClass('post-title').text(note.title)
-            //time = $('<p/>').addClass('post-meta').append($('<span/>')
-            //  .addClass('post-category post-category-design').text('Created: ' + note.created_at))
-            //header = $("<header/>").addClass('post-header')
-            //          .append(heading).append(time)
-            //$('#notes').append(
-            //  $("<section/>")
-            //    .append(header)
-            //    .append($('<div/>').addClass("post-description")
-            //      .append($('<p/>').text(note.description)))
-            //      .append($("<a/>").attr('href', note.reference_url).text(note.reference_url)
-            //    ))
-            })
-        });
+    //$.getJSON("/notes", function(notes){
+    //  $(notes).each(function(i, note){
+    //      renderNote(note);
+    //  });
+    //  $('span.note-count').text($('.post-description').length);
+    //})
 
     // Can also submit our form using post but we need to disable form default action
     // and disable usual form action
@@ -97,18 +77,17 @@ $(document).ready(function() {
               .addClass('pure-alert pure-alert-success')
               .text("Your note was created").fadeOut(600);
             renderNote(data);
-            debugger;
-            if (data["count"]) {
-              $('span.note-count').text(data["count"])
-            }
+            $('span.note-count').text($('.post-description').length)
             $("input[type=text],input[type=url],textarea").val("");
           }
       })
 
     })
 
-    // Shortcut commands for $.ajax are available as follows
-    // $.get
+    // Shortcut commands for $.ajax POST is available as follows
+    // $.post - http://api.jquery.com/jquery.post/
+    // jQuery.post( url [, data ] [, success ] [, dataType ] )
     //
+    // Difference is no error or other callbacks like the ajax command
 
 });
