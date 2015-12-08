9332169ac5d016338509221cd6b91e2dc309234d
dbkbali <david@what2do.asia>
2015-12-08 16:43:09 -0500

    base app tweaks

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // 1) add action.rb - '/notes' for json route
+  // 2) $.ajax to load notes on document ready
+  //    debugger - to explore response
+  // 3) dataType: 'json' - JSON.parse(response)
+  // 4) Appending data -
+  //    shortform '<section></section>' - '<section/>'
+  // 5) Amend ajax to shortform $.get() && $.getJSON
+
+
+
+  // 1) $.ajax for posting form to server
+  // 2) shortform $.post() command
+  //
+
+
+
+
+  // Deferreds/Promises
+  //
+  // 1) Get loading indicator working
+  // 2) Alert if server error
+  //
+
+
 });
